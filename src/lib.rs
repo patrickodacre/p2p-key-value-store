e1991df1e38db47b04f821d6b88ebfdab8307e33
Patrick O'Dacre <patrick@patrickwho.me>
2023-11-02 13:18:37 +0100

    make tests compile

--- src/lib.rs
+++ src/lib.rs
@@ -2,3 +2,23 @@
 mod error;
 pub mod prelude;
 mod utils;
+
+pub struct KvStore {}
+
+impl KvStore {
+    pub fn new() -> Self {
+        KvStore {}
+    }
+
+    pub fn set(&mut self, key: String, val: String) {
+        panic!("");
+    }
+
+    pub fn get(&self, key: String) -> Option<String> {
+        panic!();
+    }
+
+    pub fn remove(&mut self, key: String) {
+        panic!();
+    }
+}
